afcf2412091563138403945cdd69fec9c5cf0ce0
Kacer <m.d.z.s@seznam.cz>
2018-03-13 15:26:27 +0100

    initial commit - založení projektu

---
+++ app/src/main/java/cz/marw/indoorlocalization/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package cz.marw.indoorlocalization.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cz.marw.indoorlocalization.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
