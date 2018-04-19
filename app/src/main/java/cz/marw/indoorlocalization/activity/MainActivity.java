941ec18989cc870030460124cdf4802baf58f350
Kacer <m.d.z.s@seznam.cz>
2018-04-19 15:44:05 +0200

    Přidán testovací kód pro kontrolu storage permission

--- app/src/main/java/cz/marw/indoorlocalization/activity/MainActivity.java
+++ app/src/main/java/cz/marw/indoorlocalization/activity/MainActivity.java
@@ -1,7 +1,12 @@
 package cz.marw.indoorlocalization.activity;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,10 +14,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.File;
+
 import cz.marw.indoorlocalization.R;
+import cz.marw.indoorlocalization.managers.callbacks.BluetoothManagerCallback;
 import cz.marw.indoorlocalization.managers.BluetoothManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUESTCODE_STORAGE_PERMISSION = 1;
 
     private EditText etMacAddr, etScanDuration;
     private Button btnConnect, btnScan;
@@ -30,9 +40,109 @@ public class MainActivity extends AppCompatActivity {
 
         etMacAddr.setText(BluetoothManager.SENSOR_TAG_MAC);
 
-        bluetoothManager = new BluetoothManager((android.bluetooth.BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE));
+        bluetoothManager = new BluetoothManager((android.bluetooth.BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE), new BluetoothManagerCallback() {
+            @Override
+            public void onServiceDiscovered() {
+
+            }
+
+            @Override
+            public void onScanningStarted() {
+
+            }
+
+            @Override
+            public void onScanningEnded() {
+
+            }
+
+            @Override
+            public void onRadioPrintsRead() {
+
+            }
+
+            @Override
+            public void onRadioPrintsExported() {
+
+            }
+
+            @Override
+            public void onErrorOccured() {
+
+            }
+        });
 
         setListeners();
+
+        testStorage();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUESTCODE_STORAGE_PERMISSION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                    createDir();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                    System.out.println("permission denied, boo!");
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+    private void testStorage() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        REQUESTCODE_STORAGE_PERMISSION);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+            createDir();
+        }
+    }
+
+    private void createDir() {
+        File externalStorageDir = Environment.getExternalStorageDirectory();
+        File testDir = new File(externalStorageDir, "SensorTag");
+        if(!testDir.exists())
+            testDir.mkdir();
     }
 
     private void setListeners() {
