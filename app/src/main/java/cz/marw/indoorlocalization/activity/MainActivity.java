1a8252e9c81c624f480e4b873c837b8876fb2bbd
Kacer <m.d.z.s@seznam.cz>
2018-04-22 14:27:25 +0200

    Po zahájení skenu se automaticky zavře klávesnice

--- app/src/main/java/cz/marw/indoorlocalization/activity/MainActivity.java
+++ app/src/main/java/cz/marw/indoorlocalization/activity/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
@@ -211,6 +212,13 @@ public class MainActivity extends AppCompatActivity {
            scanDuration == 0 ||
            scanDuration > 65535)
             return;
+
+        //hide keyboard
+        View view = getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
 
         btnScan.setClickable(false);
 
